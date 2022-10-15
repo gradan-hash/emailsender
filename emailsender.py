80e8e2167394e4f1ec2af1f85ce7247a5de550c7
cornelius mutuku <80700768+gradan-hash@users.noreply.github.com>
2022-10-15 09:32:09 +0300

    Add files via upload

---
+++ emailsender.py
@@ -0,0 +1,40 @@
+
+from email.message import EmailMessage
+
+
+import ssl
+import smtplib
+
+
+email_sender = "corneliusmutuku55@gmail.com"
+email_password = "duxeogppswjksthp"
+
+email_receiver = "mutukunyaa@gmail.com"
+
+subject = "hello work hard brother"
+body = "usichoke mwanangu kasongee motive: "
+
+em = EmailMessage()
+
+em["From"] = email_sender
+em["To"] = email_receiver
+em["subject"] = subject
+em.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL("smtp.gmail.com" , 465, context =context) as smtp:
+  smtp.login(email_sender, email_password)
+  smtp.sendmail(email_sender, email_receiver, em.as_string())
+  
+  while True:
+    print(" Email has been send  sucesss")
+    break
+  
+  
+
+
+
+
+
+
